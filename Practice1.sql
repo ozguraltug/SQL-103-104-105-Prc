aeef6cc8e5472da30d79fc0ebb932039a9246702
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-24 18:48:22 +0300

    p1

--- Practice1.sql
+++ Practice1.sql
@@ -1,11 +1,11 @@
 CREATE TABLE ogrenciler (
-    ogrenci_no INTEGER PRIMARY KEY,
-    isim_soyad VARCHAR(255),
+    id INTEGER PRIMARY KEY,
+    isim VARCHAR(255),
     adres VARCHAR(255),
-    notu FLOAT
+    notu int
 );
 
-INSERT INTO ogrenciler (ogrenci_no, isim_soyad, adres, notu) VALUES
+INSERT INTO ogrenciler (id, isim, adres, notu) VALUES
 (1, 'Ahmet Yılmaz', 'Ankara', 80),
 (2, 'Burcu Öztürk', 'İstanbul', 90),
 (3, 'Cemal Doğan', 'İzmir', 70),
@@ -14,3 +14,12 @@ INSERT INTO ogrenciler (ogrenci_no, isim_soyad, adres, notu) VALUES
 (6, 'Fatih Demir', 'Antalya', 65),
 (7, 'Gizem Tekin', 'Konya', 75);
 
+select * from ogrenciler
+
+--Soru 1: Öğrenciler tablosundaki id ve isim sütununu getiriniz
+
+select id,isim from ogrenciler;
+
+--Soru 2: Sınav notu 80 den büyük olan öğrencilerin tüm bilgilerini listele
+
+select * from ogrenciler where notu>80;
